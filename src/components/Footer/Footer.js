952398d3bfbbd11103b75452c4e9e8c018e607e2
Chu Hoang Son <chuson1996@gmail.com>
2016-11-11 00:15:31 +0200

    Update style

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Nav from 'react-bootstrap/lib/Nav';
-import NavItem from 'react-bootstrap/lib/NavItem';
+// import Nav from 'react-bootstrap/lib/Nav';
+// import NavItem from 'react-bootstrap/lib/NavItem';
 
 export default class Footer extends Component {
   static propTypes = {};
@@ -9,16 +9,7 @@ export default class Footer extends Component {
     const styles = require('./Footer.scss');
     return (
       <div className={`well text-center m-b-0 ${styles.footer}`}>
-        <Nav
-          bsStyle="pills"
-          justified
-          activeKey={0}
-          className={styles.footerLinks}>
-          <NavItem className={`${styles.link} hvr-underline-from-center`} eventKey={3} href="https://www.facebook.com/holdmyhandifyoucan">Facebook</NavItem>
-          <NavItem className={`${styles.link} hvr-underline-from-center`} eventKey={2} href="https://github.com/chuson1996">Github</NavItem>
-          <NavItem className={`${styles.link} hvr-underline-from-center`} eventKey={1} href="https://fi.linkedin.com/in/chu-hoang-son-b92499a5">LinkedIn</NavItem>
-        </Nav>
-        © 2016 Chu Hoang Son - <a href="mailto:chuson1996@gmail.com">Contact</a>
+        © 2016 FroDev
       </div>
     );
   }
