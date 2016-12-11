80340fb1907699641384737c937ed054a948a075
Chu Hoang Son <chuson1996@gmail.com>
2016-12-11 22:12:36 +0200

    Enable cors for api

--- api/api.js
+++ api/api.js
@@ -35,6 +35,12 @@ app.use(session({
 }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // Auth
 app.use(passport.initialize());
 app.use(passport.session());
